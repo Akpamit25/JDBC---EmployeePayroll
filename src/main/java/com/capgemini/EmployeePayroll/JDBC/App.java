9704ff52992927662c3e958d2d7c715093da5902
Amit Kumar <amitnitsri25@gmail.com>
2020-10-28 23:16:23 +0530

    Master Branch

---
+++ src/main/java/com/capgemini/EmployeePayroll/JDBC/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.EmployeePayroll.JDBC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
